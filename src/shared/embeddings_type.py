b1b8e0213d18514b202abcb3f539a5560b06c6d7
David Garnitz <davidgarnitz@Davids-MacBook-Pro-2.local>
2023-08-30 12:11:19 -0700

    added new fields to support hugging face models, added unit test

--- src/shared/embeddings_type.py
+++ src/shared/embeddings_type.py
@@ -3,5 +3,5 @@ from enum import Enum
 class EmbeddingsType(Enum):
     OPEN_AI = 'open_ai'
     COHERE = 'cohere'
-    ANTHROPIC = 'anthropic'
     SELF_HOSTED = 'self_hosted'
+    HUGGING_FACE = 'hugging_face'
