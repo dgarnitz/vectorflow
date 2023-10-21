953c0705b9057ef383649466c420e566094216fc
kpriver55 <131202513+kpriver55@users.noreply.github.com>
2023-10-21 13:57:53 -0700

    Re add character chunking (#75)
    * Added exact character chunking method
    
    * Finished adding character based chunking methods

--- src/shared/chunk_strategy.py
+++ src/shared/chunk_strategy.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 class ChunkStrategy(Enum):
     EXACT = 'exact'
+    EXACT_BY_CHARACTERS = 'exact_by_characters'
     PARAGRAPH = 'paragraph'
+    PARAGRAPH_BY_CHARACTERS = 'paragraph_by_characters'
     SENTENCE = 'sentence'
+    SENTENCE_BY_CHARACTERS = 'sentence_by_characters'
     CUSTOM = 'custom'
