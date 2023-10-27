aceab1c2777cc593f2fe8d55502b217876826b79
Aleks B <aleksandir@me.com>
2023-10-27 22:37:15 +1030

    Refactor: add func to address duplicates
    1. remove unessasary trailing whitespace in file.

--- src/shared/utils.py
+++ src/shared/utils.py
@@ -1,6 +1,12 @@
 import uuid
 import requests
 import json
+import logging
+import batch_service
+from job_service import JobStatus, job_service
+import safe_db_operation
+
+
 
 def generate_uuid_from_tuple(t, namespace_uuid='6ba7b810-9dad-11d1-80b4-00c04fd430c8'):
     namespace = uuid.UUID(namespace_uuid)
@@ -17,7 +23,7 @@ def send_embeddings_to_webhook(embedded_chunks: list[dict], job):
         "X-Embeddings-Webhook-Key": job.webhook_key,
         "Content-Type": "application/json"
     }
-    
+
     data = {
         'Embeddings': embedded_chunks,
         'DocumentID': job.document_id if (hasattr(job, 'document_id') and job.document_id) else "",
@@ -25,9 +31,25 @@ def send_embeddings_to_webhook(embedded_chunks: list[dict], job):
     }
 
     response = requests.post(
-        job.webhook_url, 
-        headers=headers, 
+        job.webhook_url,
+        headers=headers,
         json=data
     )
 
     return response
+
+def update_batch_and_job_status(job_id, batch_status, batch_id):
+    try:
+        if not job_id and batch_id:
+            job = safe_db_operation(batch_service.get_batch, batch_id)
+            job_id = job.job_id
+        updated_batch_status = safe_db_operation(batch_service.update_batch_status, batch_id, batch_status)
+        job = safe_db_operation(job_service.update_job_with_batch, job_id, updated_batch_status)
+        if job.job_status == JobStatus.COMPLETED:
+            logging.info(f"Job {job_id} completed successfully")
+        elif job.job_status == JobStatus.PARTIALLY_COMPLETED:
+            logging.info(f"Job {job_id} partially completed. {job.batches_succeeded} out of {job.total_batches} batches succeeded")
+
+    except Exception as e:
+        logging.error('Error updating job and batch status: %s', e)
+        safe_db_operation(job_service.update_job_status, job_id, JobStatus.FAILED)
